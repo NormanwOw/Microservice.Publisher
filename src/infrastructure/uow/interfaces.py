72a8fd663c448f8157091c0278d151671817112f
NormanwOw <normjkeee@vk.com>
2026-01-03 21:25:42 +0500

    ADDED UnitOfWork

---
+++ src/infrastructure/uow/interfaces.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+from src.infrastructure.repositories import interfaces as i
+
+
+class IUnitOfWork(ABC):
+    outbox: i.IOutboxRepository
+
+    async def __aenter__(self):
+        raise NotImplementedError
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def commit(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def rollback(self):
+        raise NotImplementedError
