3329fdc502e4c97f1fcf5f23342f7d041c7072c1
NormanwOw <normjkeee@vk.com>
2026-01-03 21:27:22 +0500

    UPDATED Message, added factory

--- src/infrastructure/messaging/messages.py
+++ src/infrastructure/messaging/messages.py
@@ -1,6 +1,26 @@
+from typing import Self
+
 from pydantic import BaseModel
+
+from src.infrastructure.models import OutboxModel
 
 
 class Message(BaseModel):
     id: str
-    message: str
+    message: dict
+    topic: str
+
+    @classmethod
+    def factory(cls, outbox: OutboxModel) -> Self:
+        message = {
+            'message_id': str(outbox.id),
+            'command_type': outbox.action,
+            'external_reference': outbox.external_reference,
+            'payload': outbox.payload,
+            'producer': outbox.producer,
+        }
+        return cls(
+            id=str(outbox.id),
+            message=message,
+            topic=outbox.topic,
+        )
